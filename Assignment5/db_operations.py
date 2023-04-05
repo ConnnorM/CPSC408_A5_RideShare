50d65a20fb9b1854a9f818c2fde66c7f2e1d443f
Gabriel4585 <gagarcia@chapman.edu>
2023-04-05 08:19:52 -0700

    adding all files

--- Assignment5/db_operations.py
+++ Assignment5/db_operations.py
@@ -39,7 +39,7 @@ class db_operations():
     # Creates the Drivers table in the database
     def create_drivers_table(self):
         query = '''
-        CREATE TABLE drivers
+        CREATE TABLE drivers(
         driverID VARCHAR(4) NOT NULL PRIMARY KEY,
         currentRating DOUBLE,
         activeDriver BOOLEAN
@@ -51,7 +51,7 @@ class db_operations():
     # Creates the Riders table in the database
     def create_riders_table(self):
         query = '''
-        CREATE TABLE riders
+        CREATE TABLE riders(
         riderID VARCHAR(4) NOT NULL PRIMARY KEY
         );
         '''
@@ -61,7 +61,7 @@ class db_operations():
     # Creates the Rides table in the database
     def create_rides_table(self):
         query = '''
-        CREATE TABLE rides
+        CREATE TABLE rides(
         rideID INT NOT NULL PRIMARY KEY,
         driverID VARCHAR(4) NOT NULL,
         riderID VARCHAR(4) NOT NULL,
@@ -78,6 +78,10 @@ class db_operations():
         return self.cursor.fetchone() [0] #fetchone() returns first row of query, [0] returns first cell of that row
         #can give us just the value of the COUNT(*)
 
+    def insert_single_record(self,query):
+        self.cursor.execute(query)
+        self.connection.commit()
+
     #function to bulk insert records
     #runs query for each record in "records"
     def bulk_insert(self,query,records):
@@ -90,7 +94,8 @@ class db_operations():
         self.cursor.execute(query)  #results of query in the cursor
         results = self.cursor.fetchall() #fetchall() returns all rows of query in a 2d array with a bunch of arrays of length 1
         results = [i[0] for i in results]   #for everything in results make it equal to the first cell of that row and add it to results
-        results.remove(None)    #remove None values
+        if("None" in results):
+            results.remove("None")    #remove None values
         return results
 
     def name_placeholder_query(self, query, dictionary):
